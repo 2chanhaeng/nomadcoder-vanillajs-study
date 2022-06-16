755e199102cebdcd0adece01ac4e4c9e6fe5c1d9
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-06-16 15:52:45 +0900

    Study #2.2 Variables

---
+++ #2 Welcome to Javascript/#2.2 Variables/app.js
@@ -0,0 +1,23 @@
+// console.log
+console.log(545454);
+console.log("lalalala");
+console.log('lalalala');
+
+// operator
+console.log(5 + 2); // add
+console.log(5 * 2); // multiply
+console.log(5 / 2); // divide
+
+// variable
+const a = 5; // constant
+const b = 2;
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+
+// variable naming convention
+const myName = "chanhaeng"; // cammel case
+const my_name = "chanhaeng"; // snake case
+
+// string operate
+console.log("hello " + myName);
