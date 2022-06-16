8992855b0138739dcf5b80c32d235fe72afc35be
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-06-16 16:11:22 +0900

    Study #2.4 Booleans

---
+++ #2 Welcome to Javascript/#2.4 Booleans/app.js
@@ -0,0 +1,9 @@
+// boolean
+const isLoggedIn = true; // boolean
+console.log(isLoggedIn);
+
+// null, undefined
+const isNull = null; // null
+console.log(isNull);
+let someting; // undefined
+console.log(someting);
