ebc7786215e2a9cfc330dbd9ab419dc13ecc2f80
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-06-22 15:07:43 +0900

    Study #3.1 HTML in Javascript

---
+++ #3 Javascript on the Browser/#3.1 HTML in Javascript/app.js
@@ -0,0 +1,10 @@
+// Get HTML Element by JS
+const title = document.getElementById("title");
+console.log(title);
+console.dir(title);
+console.log(title.id);
+console.log(title.className);
+
+
+// Set HTML Element by JS
+title.innerText = "Got you!";
