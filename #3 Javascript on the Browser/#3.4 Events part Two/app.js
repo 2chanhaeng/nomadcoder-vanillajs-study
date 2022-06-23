e39ab3be56e6b1a7e62736cd44a69d1e8fbb912b
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-06-23 15:46:55 +0900

    Study #3.4 Events part Two

---
+++ #3 Javascript on the Browser/#3.4 Events part Two/app.js
@@ -0,0 +1,15 @@
+// listen events
+const title = document.querySelector("div.hello:first-child h1");
+function handleTitleClick(){
+    title.style.color = "blue";
+}
+function handleMouseEnter(){
+    title.innerHTML = "Mouse is here!";
+}
+function handleMouseLeave(){
+    title.innerHTML = "Mouse is gone!";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
