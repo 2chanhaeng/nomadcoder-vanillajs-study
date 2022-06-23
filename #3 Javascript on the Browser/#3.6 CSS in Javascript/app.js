1a4220320a16025e8a4d71fed93fded1b510399c
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-06-23 16:09:01 +0900

    Study #3.6 CSS in Javascript

---
+++ #3 Javascript on the Browser/#3.6 CSS in Javascript/app.js
@@ -0,0 +1,14 @@
+// if else statements in function
+const h1 = document.querySelector("div.hello:first-child h1");
+function handleH1Click(){
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "blue"){
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
+}
+
+h1.onclick = handleH1Click;
